2144aa03e878503473b849420f4d08508fa37e9a
Alexey Kloktunov <kloktun@gmail.com>
2023-07-08 23:51:48 +0400

    feat(helper): make text optional field

--- src/components/helper/index.tsx
+++ src/components/helper/index.tsx
@@ -4,7 +4,7 @@ import classnames from "classnames";
 export interface HelperProps {
 
     children: React.ReactElement;
-    text: string | React.ReactElement;
+    text?: string | React.ReactElement;
     error?: string | React.ReactElement;
     suffix?: string | React.ReactElement;
 
@@ -13,24 +13,38 @@ export interface HelperProps {
 const Helper = ({ children, text, error, suffix }: HelperProps) => {
 
     const content = error ?? text;
+    const hasContent = !!content;
+    const hasSuffix = !!suffix;
     const hasError = !!error;
+
+    const showHelper = hasContent || hasSuffix;
 
     return <div className='flex flex-col gap-1'>
         
         {children}
 
-        <div className={classnames('flex flex-row text-sm items-center justify-between gap-1', {
-            'text-front-hint fill-front-hint': !hasError,
-            'text-error fill-error': hasError,
-        })}>
+        { showHelper &&
 
-            <div className='flex flex-row items-center gap-1'>
-                {content}
+            <div className={classnames('flex flex-row text-sm items-center justify-between gap-1', {
+                'text-front-hint fill-front-hint': !hasError,
+                'text-error fill-error': hasError,
+            })}>
+
+                { hasContent &&
+                    <div className='flex flex-row items-center gap-1'>
+                        {content}
+                    </div>
+                }
+
+                { hasSuffix &&
+                    <div className='flex flex-row items-center'>
+                        {suffix}
+                    </div>
+                }
+
             </div>
 
-            { suffix && <div className='flex flex-row items-center'>{suffix}</div> }
-
-        </div>
+        }
     </div>
 
 }
