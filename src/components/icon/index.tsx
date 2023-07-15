ad8d2b828016f25ad2e5775445f9f33e471ec17d
Alexey Kloktunov <kloktun@gmail.com>
2023-07-16 02:08:39 +0400

    feat(icon): add color prop to icon component

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -7,12 +7,12 @@ export type IconProp = string | JSX.Element | React.ReactNode | React.ReactEleme
 interface Props {
 
     icon?: IconProp;
-
+    color?: string;
     size?: ControlSize | "huge";
 
 }
 
-const Icon = ({ icon, size = "default" }: Props) => {
+const Icon = ({ icon, size = "default", color }: Props) => {
 
     if(!icon){
         return null;
@@ -30,15 +30,22 @@ const Icon = ({ icon, size = "default" }: Props) => {
     return (
         <div className={
 
-            classnames('icon kl-flex kl-items-center kl-justify-center kl-leading-none kl-fill-current kl-text-current', {
-
+            classnames('icon kl-flex kl-items-center kl-justify-center kl-leading-none', {
+                'kl-fill-current kl-text-current': !color,
                 'kl-w-12 kl-h-12 kl-text-5xl': size == 'huge',
                 'kl-w-6 kl-h-6 kl-text-2xl': size == 'large',
                 'kl-w-5 kl-h-5 kl-text-xl': size == 'medium' || size == 'default' || size == 'small',
                 'kl-w-4 kl-h-4 kl-text-base': size == 'mini',
         
             })
-        } >
+        } 
+            style={
+                {
+                    color: color,
+                    fill: color
+                }
+            }
+        >
             {el}
         </div>
     );
