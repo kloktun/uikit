f2db19e817721c7f8495c1448ce3483fec303040
Alexey Kloktunov <kloktun@gmail.com>
2024-05-18 20:43:54 +0400

    fix(textarea): fix autosize textarea on init

--- src/components/textarea/index.tsx
+++ src/components/textarea/index.tsx
@@ -1,4 +1,10 @@
-import React, { forwardRef, useCallback, useEffect, useRef, useState } from "react";
+import React, {
+	forwardRef,
+	useCallback,
+	useEffect,
+	useRef,
+	useState,
+} from "react";
 import classnames from "classnames";
 import { ControlsProps } from "../../common/controls.type";
 import { useCombinedRefs } from "../../hooks/combinedrefs";
@@ -6,128 +12,130 @@ import { useCombinedRefs } from "../../hooks/combinedrefs";
 type P = Omit<React.HTMLProps<HTMLTextAreaElement>, "size"> & ControlsProps;
 
 interface Props extends P {
-
-    value?: string;
-    autosize?: boolean | { minRows?: number, maxRows?: number },
-
+	value?: string;
+	autosize?: boolean | { minRows?: number; maxRows?: number };
 }
 
-const Textarea = forwardRef<HTMLTextAreaElement, Props>(({ value, onChange, disabled, success, warning, error, size = "default", autosize = { minRows: 2, maxRows: 5 }, ...restProps }, ref) => {
+const Textarea = forwardRef<HTMLTextAreaElement, Props>(
+	(
+		{
+			value,
+			onChange,
+			disabled,
+			success,
+			warning,
+			error,
+			size = "default",
+			autosize = { minRows: 2, maxRows: 5 },
+			...restProps
+		},
+		ref
+	) => {
+		const textareaElement = useRef<HTMLTextAreaElement>(null);
+		const combinedRef = useCombinedRefs<HTMLTextAreaElement>(
+			textareaElement,
+			ref
+		);
 
-    const textareaElement = useRef<HTMLTextAreaElement>(null);
-    const combinedRef = useCombinedRefs<HTMLTextAreaElement>(textareaElement, ref);
-   
-    useEffect(() => {
+		useEffect(() => {
+			if (textareaElement.current) {
+				updateSize();
+			}
+		}, []);
 
-        updateSize();
+		useEffect(() => {
+			updateSize();
+		}, [value, autosize, textareaElement]);
 
-    }, [value, textareaElement]);
+		const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+			if (onChange) {
+				onChange(e);
+			}
 
-    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+			updateSize();
+		};
 
-        if(onChange){
-            onChange(e);
-        }
+		const updateSize = () => {
+			if (!autosize) {
+				return;
+			}
 
-        updateSize();
-        
-    }
+			if (!textareaElement.current) {
+				return;
+			}
 
-    const updateSize = () => {
+			setupHeightLimits();
 
-        if(!autosize){
-            return;
-        }
+			const styles = getComputedStyle(textareaElement.current);
+			const borderTopWidth = parseInt(styles.borderTopWidth);
+			const borderBottomWidth = parseInt(styles.borderBottomWidth);
+			const borderVeritalWidth = borderTopWidth + borderBottomWidth;
 
-        if(!textareaElement.current){
-            return;
-        }
-    
-        setupHeightLimits();
-        
-        const styles = getComputedStyle(textareaElement.current);
-        const borderTopWidth = parseInt(styles.borderTopWidth);
-        const borderBottomWidth = parseInt(styles.borderBottomWidth);
-        const borderVeritalWidth = borderTopWidth + borderBottomWidth;
+			textareaElement.current.style.height = "0";
+			textareaElement.current.style.height = `${
+				textareaElement.current.scrollHeight + borderVeritalWidth
+			}px`;
+		};
 
-        textareaElement.current.style.height = "0";
-        textareaElement.current.style.height = `${textareaElement.current.scrollHeight + borderVeritalWidth}px`;
-    
-    }
-    
-    const setupHeightLimits = () => {
-    
-        if(!autosize || typeof autosize == "boolean"){
-            return;
-        }
-        
-        if(!textareaElement.current){
-            return;
-        }
-    
-        const styles = getComputedStyle(textareaElement.current);
-        const lineHeight = parseInt(styles.lineHeight);
-        const borderTopWidth = parseInt(styles.borderTopWidth);
-        const borderBottomWidth = parseInt(styles.borderBottomWidth);
-        const borderVeritalWidth = borderTopWidth + borderBottomWidth;
-        const paddingTop = parseInt(styles.paddingTop);
-        const paddingBottom = parseInt(styles.paddingBottom);
-        const verticalPadding = paddingTop + paddingBottom;
+		const setupHeightLimits = () => {
+			if (!autosize || typeof autosize == "boolean") {
+				return;
+			}
 
-        let { minRows, maxRows } = autosize;
-    
-        if(minRows){
-    
-            textareaElement.current.style.minHeight = `${lineHeight * minRows + verticalPadding + borderVeritalWidth}px`;
-    
-        }
-    
-        if(maxRows){
-    
-            textareaElement.current.style.maxHeight = `${lineHeight * maxRows + verticalPadding + borderVeritalWidth}px`;
-    
-        }
-    
-    }
-    
-    return (
-        <textarea
-            ref={combinedRef}
-            value={value}
-            onChange={handleChange}
-            {...restProps}
+			if (!textareaElement.current) {
+				return;
+			}
 
-            className={
-                classnames(
-                    restProps.className,
-                    'kl-rounded-control editable-control-border editable-control-background editable-control-background kl-resize-none', {
+			const styles = getComputedStyle(textareaElement.current);
+			const lineHeight = parseInt(styles.lineHeight);
+			const borderTopWidth = parseInt(styles.borderTopWidth);
+			const borderBottomWidth = parseInt(styles.borderBottomWidth);
+			const borderVeritalWidth = borderTopWidth + borderBottomWidth;
+			const paddingTop = parseInt(styles.paddingTop);
+			const paddingBottom = parseInt(styles.paddingBottom);
+			const verticalPadding = paddingTop + paddingBottom;
 
-                        
+			let { minRows, maxRows } = autosize;
 
-                        'success': success,
-                        'error': error,
-                        'warning': warning,
-                        'kl-opacity-50': disabled,
+			if (minRows) {
+				textareaElement.current.style.minHeight = `${
+					lineHeight * minRows + verticalPadding + borderVeritalWidth
+				}px`;
+			}
 
-                        'kl-px-3 kl-py-3': size == "large",
-                        'kl-px-3 kl-py-2.5': size == "medium",
-                        'kl-px-3 kl-py-2': size == "default",
-                        'kl-px-2.5 kl-py-1.5': size == "small",
-                        'kl-px-2 kl-py-1': size == "mini",
+			if (maxRows) {
+				textareaElement.current.style.maxHeight = `${
+					lineHeight * maxRows + verticalPadding + borderVeritalWidth
+				}px`;
+			}
+		};
 
+		return (
+			<textarea
+				ref={combinedRef}
+				value={value}
+				onChange={handleChange}
+				{...restProps}
+				className={classnames(
+					restProps.className,
+					"kl-rounded-control editable-control-border editable-control-background editable-control-background kl-resize-none",
+					{
+						success: success,
+						error: error,
+						warning: warning,
+						"kl-opacity-50": disabled,
 
-
-                    }
-                )
-            }
-
-            disabled={disabled}
-        >
-
-        </textarea>
-
-    );
-
-});
+						"kl-px-3 kl-py-3": size == "large",
+						"kl-px-3 kl-py-2.5": size == "medium",
+						"kl-px-3 kl-py-2": size == "default",
+						"kl-px-2.5 kl-py-1.5": size == "small",
+						"kl-px-2 kl-py-1": size == "mini",
+					}
+				)}
+				disabled={disabled}
+			></textarea>
+		);
+	}
+);
 
 export default Textarea;
